ebf2bcd2c207f02a765748659e5a8c1f5d3b151b
snazari <snazari@users.noreply.github.com>
2025-03-10 05:34:29 -0400

    directory resumes

---
+++ data/candidates/resumes/AI_ML_New_Hire_Candidates.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bee81eca32da7cf8510d97e3caa63208ff3ef85bc608007f3d16b6135a2affbe
+size 1245
---
+++ data/candidates/resumes/process_resume_files_arpah.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d4070efd69a6f1dee08e82fd73122e90777172a7c136f1fab7e622e303e3213
+size 2265
